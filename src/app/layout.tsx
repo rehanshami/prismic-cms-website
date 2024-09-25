dd5800934b021593c2f69909ed4f576ecf011be3
rehanshami <77270784+rehanshami@users.noreply.github.com>
2024-09-25 13:44:32 +1200

    feat: Added Fonts and cleared boilerplate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,21 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import clsx from 'clsx'
+import { Nunito, Nunito_Sans } from 'next/font/google';
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const nunito = Nunito ({
+  subsets: ['latin'],
+  variable: '--font-nunito',
+  display: 'swap'
+})
+
+const nunitoSans = Nunito_Sans ({
+  subsets: ['latin'],
+  variable: '--font-nunito-sans',
+  display: 'swap'
+})
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +28,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={clsx(nunito.variable, nunitoSans.variable)}>
+      <body>
         {children}
       </body>
     </html>
