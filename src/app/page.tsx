1b4db4c038e1502350f1324a51893a477a723cb2
rehanshami <77270784+rehanshami@users.noreply.github.com>
2024-09-25 19:38:29 +1200

    feat: Add fallback meta data to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    
-      <main className="">
-       <h1 className="font-display text-5xl font-bold">Hello Woraald!</h1>
-      </main>
-    
-
+    <main className="">
+      <h1 className="font-display text-5xl font-bold">Hello Woraald!</h1>
+    </main>
   );
 }
