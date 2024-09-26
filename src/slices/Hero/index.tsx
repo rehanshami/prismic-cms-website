403e642000d994a82c2da64414e6510701b23e2f
rehanshami <77270784+rehanshami@users.noreply.github.com>
2024-09-26 23:41:04 +1200

    feat: Create Hero slice and helper components

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,7 +1,31 @@
 import { Content } from "@prismicio/client";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
-import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import {
+  JSXMapSerializer,
+  PrismicRichText,
+  SliceComponentProps,
+} from "@prismicio/react";
+import Bounded from "@/components/Bounded";
+import Button from "@/components/Button";
+import Heading from "@/components/Heading";
 
+const components: JSXMapSerializer = {
+  heading1: ({ children }) => (
+    <Heading
+      as="h1"
+      size="xl"
+      className="md:mb-8 mb-4 mt-12 first:mt-0 last:mb-0"
+    >
+      {children}
+    </Heading>
+  ),
+
+  paragraph: ({ children }) => (
+    <p className="text-2xl text-center font-normal leading-10 font-body text-slate-600 mb-4 md:mb-8 max-w-md">
+      {children}
+    </p>
+  ),
+};
 /**
  * Props for `Hero`.
  */
@@ -12,18 +36,26 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
   return (
-    <section
+    <Bounded
+      className="px-4 py-10 md:py-14 md:px-6 lg:py-16"
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <PrismicRichText field={slice.primary.heading} />
-      <PrismicRichText field={slice.primary.body} />
-      {slice.primary.button_text}
-      <PrismicNextLink field={slice.primary.button_link}>
-        {slice.primary.button_text}
-      </PrismicNextLink>
-      <PrismicNextImage field={slice.primary.image} />
-    </section>
+      <div className="grid grid-cols-1 place-items-center text-center">
+        <PrismicRichText
+          field={slice.primary.heading}
+          components={components}
+        />
+        <PrismicRichText field={slice.primary.body} components={components} />
+        <Button field={slice.primary.button_link} className="mb-8 md:mb-10">
+          {slice.primary.button_text}
+        </Button>
+        <PrismicNextImage
+          field={slice.primary.image}
+          className="drop-shadow-xl max-w-4xl w-full"
+        />
+      </div>
+    </Bounded>
   );
 };
 
